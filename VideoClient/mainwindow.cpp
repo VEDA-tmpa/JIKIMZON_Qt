cf2bd8dfed567a072ea4a84e4798f156ae9a6f5e
jjeongni03 <160263738+jjeongni03@users.noreply.github.com>
2024-11-18 00:20:37 +0900

    fix: MainWindow에서 RecordingWidget 초기화 수정
    - VideoStreamWidget의 getCurrentFrame 함수 추가에 따라 MainWindow의 RecordingWidget 초기화 코드 수정
    - VideoStreamWidget과의 연동 문제 해결

--- VideoClient/mainwindow.cpp
+++ VideoClient/mainwindow.cpp
@@ -1,23 +1,32 @@
-#include "mainwindow.h"
+ #include "mainwindow.h"
 #include "ui_mainwindow.h"
 #include "VideoStreamWidget.h"
+#include "recordingwidget.h"
+#include "cameraswitchwidget.h"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow),
-    tcpSocket(new QTcpSocket(this)) // tcpSocket을 MainWindow에서 생성
+    , ui(new Ui::MainWindow)
+    , tcpSocket(new QTcpSocket(this)) // tcpSocket을 MainWindow에서 생성
+    , centralWidget(new QWidget(this))
+    , mainLayout(new QVBoxLayout(centralWidget))
 {
     ui->setupUi(this);
 
-    // VideoStreamWidget에서 tcpSocket을 사용하도록 전달
-    videoStreamWidget = new VideoStreamWidget(tcpSocket);  // VideoStreamWidget에서 tcpSocket 사용
+    // 네트워크 연결 초기화
+    setupNetwork();
 
-    // QStackedWidget 설정 (UI에 위젯 추가)
-    QStackedWidget *stackedWidget = new QStackedWidget(this);
-    stackedWidget->addWidget(videoStreamWidget);
+    // UI 초기화
+    initializeUI();
+}
 
-    setCentralWidget(stackedWidget);  // stackedWidget을 메인 윈도우에 배치
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
 
+void MainWindow::setupNetwork()
+{
     // 네트워크 연결 시도
     tcpSocket->connectToHost("127.0.0.1", 9999);
 
@@ -27,8 +36,22 @@ MainWindow::MainWindow(QWidget *parent)
     }
 }
 
-MainWindow::~MainWindow()
+void MainWindow::initializeUI()
 {
-    delete ui;
-    delete tcpSocket;
+    // 위젯 생성
+    videoStreamWidget = new VideoStreamWidget(tcpSocket, this);
+    recordingWidget = new RecordingWidget(videoStreamWidget, this);
+    cameraSwitchWidget = new CameraSwitchWidget(this);
+
+    // 비디오 스트림 크기 설정
+    videoStreamWidget->setMinimumSize(640, 480); // 비디오 스트림의 최소 크기 설정
+
+    // 레이아웃 구성
+    mainLayout->addWidget(cameraSwitchWidget); // 카메라 전환 위젯 추가
+    mainLayout->addWidget(videoStreamWidget);  // 비디오 스트림 위젯 추가
+    mainLayout->addWidget(recordingWidget);    // 녹화 위젯 추가
+
+    // 중앙 위젯 설정
+    centralWidget->setLayout(mainLayout);
+    setCentralWidget(centralWidget); // QMainWindow에 중앙 위젯 배치
 }
--- VideoClient/mainwindow.h
+++ VideoClient/mainwindow.h
@@ -3,13 +3,13 @@
 
 #include <QMainWindow>
 #include <QTcpSocket>
-#include <QStackedWidget>
-#include "VideoStreamWidget.h"  // VideoStreamWidget 헤더
+#include <QVBoxLayout>
+#include "VideoStreamWidget.h"
+#include "recordingwidget.h"
+#include "cameraswitchwidget.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,16 +17,22 @@ class MainWindow : public QMainWindow
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
-    Ui::MainWindow *ui;
+    void setupNetwork();   // 네트워크 연결 초기화 함수
+    void initializeUI();   // UI 초기화 함수
 
-    // VideoStreamWidget에서 사용할 tcpSocket 생성
-    QTcpSocket *tcpSocket;
+    Ui::MainWindow *ui;               // UI 포인터
+    QTcpSocket *tcpSocket;            // TCP 소켓
+    QWidget *centralWidget;           // 중앙 위젯
+    QVBoxLayout *mainLayout;          // 메인 레이아웃
 
-    // 위젯들
+    // 커스텀 위젯
     VideoStreamWidget *videoStreamWidget;
+    RecordingWidget *recordingWidget;
+    CameraSwitchWidget *cameraSwitchWidget;
 };
+
 #endif // MAINWINDOW_H
