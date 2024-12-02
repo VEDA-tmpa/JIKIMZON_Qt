fe9104bf01bb481fa55054c63bb3c55c05d99d25
k1sihyeon <tlgus530@naver.com>
2024-12-02 11:14:45 +0900

    feat: Deserialize header, body, frame

--- JIKIMZON_Qt.pro
+++ JIKIMZON_Qt.pro
@@ -9,13 +9,20 @@ CONFIG += c++17
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
 SOURCES += \
+    body.cpp \
     decodehandler.cpp \
+    frame.cpp \
+    header.cpp \
     main.cpp \
     mainwindow.cpp \
     videostreamplayer.cpp
 
 HEADERS += \
+    ISerializeInterface.h \
+    body.h \
     decodehandler.h \
+    frame.h \
+    header.h \
     mainwindow.h \
     videostreamplayer.h
 
---
+++ header.cpp
@@ -0,0 +1,95 @@
+#include "frame.h"
+
+//#include <arpa/inet.h>
+#include <QtEndian>
+#include <iostream>
+#include <cstring>
+#include <cstring>
+
+//네트워크는 big-endian이 사용됨
+//hton : to big-endian
+//ntoh : to little-endian
+
+namespace frame
+{
+    Header::Header(HeaderStruct& header)
+        : mHeader(header)
+    {
+    }
+    
+    void Header::Serialize(std::vector<uint8_t>& OUT buffer) const
+    {
+        buffer.clear();
+        buffer.resize(sizeof(HeaderStruct));
+
+        HeaderStruct header = mHeader;
+        header.frameId = qToBigEndian(header.frameId);
+        header.bodySize = qToBigEndian(header.bodySize);
+        header.imageWidth = qToBigEndian(header.imageWidth);
+        header.imageHeight = qToBigEndian(header.imageHeight);
+
+        std::memcpy(buffer.data(), reinterpret_cast<void*>(&header), sizeof(HeaderStruct));
+    }
+
+    std::vector<uint8_t> Header::Serialize() const
+    {
+        std::vector<uint8_t> buffer(sizeof(HeaderStruct));
+
+        this->Serialize(buffer);
+
+        return buffer;
+    }
+
+    void Header::Deserialize(std::vector<uint8_t>& buffer)
+    {
+        if (buffer.size() < sizeof(HeaderStruct))
+        {
+            throw std::runtime_error("Buffer size is too small for a valid Header.");
+        }
+
+        HeaderStruct header;
+        std::memcpy(&header, buffer.data(), sizeof(HeaderStruct));
+
+        header.frameId = qToLittleEndian(header.frameId);
+        header.bodySize = qToLittleEndian(header.bodySize);
+        header.imageWidth = qToLittleEndian(header.imageWidth);
+        header.imageHeight = qToLittleEndian(header.imageHeight);
+
+        mHeader = header;
+    }
+
+    uint32_t Header::GetFrameId() const
+    {
+        return mHeader.frameId;
+    }
+
+    uint32_t Header::GetBodySize() const
+    {
+        return mHeader.bodySize;
+    }
+
+    uint16_t Header::GetImageWidth() const
+    {
+        return mHeader.imageWidth;
+    }
+
+    uint16_t Header::GetImageHeight() const
+    {
+        return mHeader.imageHeight;
+    }
+
+    uint8_t Header::GetImageFormat() const
+    {
+        return mHeader.imageFormat;
+    }
+
+    std::string Header::GetTimestamp() const
+    {
+        return std::string(mHeader.timestamp);
+    }
+
+    void Header::SetHeader(HeaderStruct& header)
+    {
+        mHeader = header;
+    }
+}
