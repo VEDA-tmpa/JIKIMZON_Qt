146393743e377474c720a17d442be7f2f0406ede
k1sihyeon <tlgus530@naver.com>
2024-12-03 22:21:31 +0900

    fix: decrypt resize

--- JIKIMZON_Qt/src/decryptor.cpp
+++ JIKIMZON_Qt/src/decryptor.cpp
@@ -11,7 +11,7 @@ Decryptor::Decryptor()
         qDebug() << "EVP_CIPHER_CTX_new failed!";
     }
 
-    QString filepath = ":/res/res/keyfile.bin";
+    QString filepath = ":/res/res/keyfile_tmp.bin";
     LoadKey(filepath);
 }
 
@@ -24,42 +24,47 @@ Decryptor::~Decryptor()
 }
 
 void Decryptor::Decrypt(QString& nounceStr, const QByteArray& encryptedData, OUT QByteArray& decryptedData)
-{
+{   
+    decryptedData.clear();
+    decryptedData.resize(encryptedData.size());
+
     unsigned char iv[12];
     std::memcpy(iv, reinterpret_cast<const unsigned char*>(nounceStr.right(12).toStdString().c_str()), 12);
 
-    decryptedData.clear();
-
     if (EVP_DecryptInit_ex(mCtx, EVP_chacha20(), nullptr,
-                           reinterpret_cast<const unsigned char*>(mKey.data()),
-                           iv) != 1)
+                        reinterpret_cast<const unsigned char*>(mKey.data()),
+                        iv) != 1)
     {
         qDebug() << "EVP_DecryptInit_ex failed!";
         return;
     }
 
     int outLen;
+    int totalLen = 0;
     if (EVP_DecryptUpdate(mCtx, reinterpret_cast<unsigned char*>(decryptedData.data()), &outLen,
-                          reinterpret_cast<const unsigned char*>(encryptedData.data()), encryptedData.size()) != 1)
+                        reinterpret_cast<const unsigned char*>(encryptedData.data()), encryptedData.size()) != 1)
     {
         qDebug() << "EVP_DecryptUpdate failed!";
         return;
     }
 
-    if (EVP_DecryptFinal_ex(mCtx, reinterpret_cast<unsigned char*>(decryptedData.data()) + outLen, &outLen) != 1)
+    totalLen += outLen;
+
+    if (EVP_DecryptFinal_ex(mCtx, reinterpret_cast<unsigned char*>(decryptedData.data()) + totalLen, &outLen) != 1)
     {
         qDebug() << "EVP_DecryptFinal_ex failed!";
         return;
     }
 
-    qDebug() << "Decrypt Done!";    
-    qDebug() << "Decrypted data size: " << decryptedData.size();
+    totalLen += outLen;
+    decryptedData.resize(totalLen);
 }
 
 bool Decryptor::LoadKey(const QString& filePath)
 {
     QFile file(filePath);
-    if (!file.open(QIODevice::ReadOnly)) {
+    if (!file.open(QIODevice::ReadOnly))
+    {
         qDebug() << "Failed to open key file.";
         return false;
     }
@@ -67,8 +72,15 @@ bool Decryptor::LoadKey(const QString& filePath)
     mKey = file.readAll();
     file.close();
 
-    if (mKey.isEmpty()) {
+    if (mKey.isEmpty())
+    {
         qDebug() << "Key file is empty.";
+        return false;
+    }
+
+    if (mKey.size() != 32)
+    {
+        qDebug() << "Invalid key size: " << mKey.size();
         return false;
     }
 
